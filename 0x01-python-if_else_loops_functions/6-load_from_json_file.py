0ba441994ea954e5bdc43fa6152215211413ccad
Bhavesh Patel <tonyb760@gmail.com>
2023-06-06 22:14:57 +0300

    Task 0

--- 0x01-python-if_else_loops_functions/6-load_from_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module defines a JSON file-reading function"""
-import json
-
-
-def load_from_json_file(filename):
-    """Creates a Python object from a given JSON file"""
-    with open(filename) as f:
-        return json.load(f)
